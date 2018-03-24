19d01f04085b53938c768d3905319fe388c307b4
Isaac Massot <isaac.mg@coditramuntana.com>
2018-03-24 09:33:57 +0100

    wip working tests

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 source 'https://rubygems.org'
 
+ruby RUBY_VERSION
+
 # Specify your gem's dependencies in kaminari.gemspec
 gemspec
+
+gem "decidim", "~> 0.9.2"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,3 @@ ENV["ENGINE_NAME"] = File.dirname(__dir__).split("/").last
 Decidim::Dev.dummy_app_path = File.expand_path(File.join(Dir.pwd, "spec", "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
-
-require "support/genesys_i3_authorization_handler_stubs"
-RSpec.configure do |config|
-  config.include GenesysI3AuthorizationHandlerStubs
-end
